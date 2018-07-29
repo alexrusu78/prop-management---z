ac0ec2c2d201d8d2ad2e29cac0a2dbd83532685a
Alexandru RUSU <alexrusu78@yahoo.com>
2018-07-29 21:44:35 +0300

    git commit -m

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,14 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'; 
+
+import * as actions from '../../actions';
+
+import SigninForm from './signinForm';
 
 class Signin extends Component {
+
+    componentDidMount() {
+        this.props.updateHeader('Welcome to HOA Manager!', 'Please login to continue', false);
+    }
+
+    onSubmit = (fields) => {
+        this.props.signIn(fields, () => {
+            this.props.history.push('/dashboard');
+        })
+    }
+
     render() {
         return (
             <div className='sign-in'>
-                .... signin
+                <SigninForm onSubmit={(event) => this.onSubmit(event)}/>
             </div>
         )
     }
-
 }
 
-export default Signin;
+export default connect(null, actions)(Signin);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,18 @@
 import { combineReducers } from 'redux';
 
+import { reducer as form } from 'redux-form';
+
+import auth from './authReducer';
+import newsletters from './newsletterReducer';
+import requests from './requestsReducer';
+import header from './headerReducer';
+
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  form,
+  auth,
+  newsletters,
+  requests,
+  header
 });
 
 export default rootReducer;
